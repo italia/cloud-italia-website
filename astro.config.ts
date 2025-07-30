fd910821c74a56875bb73386e8c2835664532c30
Giorgio Mandolini <g.mandolini@gmail.com>
2025-07-30 18:27:51 +0200

    feat: adds vercel adapter + ssg & ssr example

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import { resolve } from "path";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   vite: {
     resolve: {
       alias: {
