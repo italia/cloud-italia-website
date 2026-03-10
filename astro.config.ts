a623504b056eb7f0e6b26ef8458293532b301d9e
mariateresagraziano <marytex.dev@gmail.com>
2026-03-10 09:50:08 +0100

    hotfix: fix fonts preload

--- astro.config.ts
+++ astro.config.ts
@@ -41,7 +41,6 @@ export default defineConfig({
         "@splidejs/splide/src/css/core/index": resolve(
           "node_modules/@splidejs/splide/src/css/core/index.scss",
         ),
-        "@font-assets": "/node_modules/bootstrap-italia/dist/fonts",
         "@bootstrap-src": "/node_modules/bootstrap-italia/src",
       },
     },
