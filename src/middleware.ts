e5dab6a43fb3b9c54a85fa9eaae93975ab20aff2
Lorezz <point.point@gmail.com>
2026-05-29 18:39:12 +0200

    chore: try auth proteced

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,7 @@ export const onRequest = defineMiddleware((context, next) => {
     if (!isAuthenticated) {
       return new Response("Unauthorized", {
         status: 401,
-        headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+        headers: { "WWW-Authenticate": 'Basic realm="Protected"' },
       });
     }
   }
